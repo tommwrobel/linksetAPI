fcd13a15159538b912e4a8d835dc35fddf7eeb2c
Tomek <tomm.wrobel@gmail.com>
2020-04-29 08:47:24 +0200

    Initial commit

---
+++ src/main/java/com/linkset/LinksetAPI/LinksetApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkset.LinksetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkset/LinksetAPI/LinksetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkset.LinksetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
